ed97bf4ca1041be2818690b65d61b1fcc95871e5
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-16 09:25:05 +0100

    added dotenv and infura_goerli_url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_GOERLI_URL = process.env.INFURA_GOERLI_URL || 'https://eth-goerli';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey';
 module.exports = {
-  solidity: "0.8.17",
+	solidity: '0.8.17',
+	networks: {
+		goerli: {
+			url: INFURA_GOERLI_URL,
+			accounts: [PRIVATE_KEY],
+			chainId: 5,
+		},
+	},
 };
